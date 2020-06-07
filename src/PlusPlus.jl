b680a47bfb7e33e54f0290e2328f43de9128f198
Mason Protter <mason.protter@icloud.com>
2020-06-07 17:01:23 -0600

    support tuple concatenation

--- src/PlusPlus.jl
+++ src/PlusPlus.jl
@@ -6,4 +6,11 @@ export (++)
 
 ++(args...) = vcat(args...)
 
+++(x) = x
+++(x::Tuple, y::Tuple) = (x..., y...)
+++(x::Tuple, y::Tuple, z...) = (x..., ++(y, z...)...)
+
+++(x::Tuple, y) = collect(x) ++ y
+++(x, y::Tuple) = x ++ collect(y)
+
 end
