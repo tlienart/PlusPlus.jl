2c337183869e3ffdc795a7b47918acfe527a36ff
tlienart <tlienart@me.com>
2020-06-21 12:56:45 +0200

    removing orig folder

--- page/__site/assets/index/code/ex.jl
+++
@@ -1,6 +0,0 @@
-# This file was generated, do not modify it. # hide
-using DataFrames
-using PlusPlus
-df = DataFrame(A = 1:4, B = ["M", "F", "F", "M"])
-df2 = DataFrame(A = 1:4, B = ["M", "F", "F", "M"])
-df ++ df2
